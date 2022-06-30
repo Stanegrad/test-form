301102efe6f61c3eab974301b4a4327facd8f340
Stanegrad <mayanknarang88@gmail.com>
2022-06-30 12:38:18 +0530

    test for form

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const tl = gsap.timeline({default: {duration: 0.15}})
 //Submit button
-const button = document.querySelector("button");
-const tl3 = gsap.timeline({
-  defaults: { duration: 0.50, ease: "Power2.easeOut" },
-});
+// const button = document.querySelector("button");
+// const tl3 = gsap.timeline({
+//   defaults: { duration: 0.50, ease: "Power2.easeOut" },
+// });
 
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  tl3.to(".maindiv", {
-    y: 30,
-    opacity: 0,
-    pointerEvents: "none",
-  });
-  tl3.to(".login-box h2", {opacity: 0 },);
-  tl3.to(".login-box", { scale: 0.8 }, "<");
-  tl3.fromTo(".submitted", { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
-});
+// button.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   tl3.to(".maindiv", {
+//     y: 30,
+//     opacity: 0,
+//     pointerEvents: "none",
+//   });
+//   tl3.to(".login-box h2", {opacity: 0 },);
+//   tl3.to(".login-box", { scale: 0.8 }, "<");
+//   tl3.fromTo(".submitted", { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+// });
